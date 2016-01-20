cc94cbce5c8b439ffd6fbc63a7377cf7521845f0
Tomasz Klim <github@tomaszklim.pl>
2016-01-20 13:16:37 +0100

    minor change

--- driver/load.sh
+++ driver/load.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-path=/etc/local/.config
+path=/etc/local/.dns
 zone=$1
 
 cat $path/zone.all $path/zone.$zone $path/zone.$zone-dynamic 2>/dev/null |grep -v ^$ |grep -v "^#" |awk '{ print $1 " " $2 " " $3 }'
