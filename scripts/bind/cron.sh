84631956f4d2cd13d4a967eebc400cb5ba36140c
Tomasz Klim <github@tomaszklim.pl>
2020-04-09 10:36:53 +0200

    support for non-standard ports in bind driver

--- scripts/bind/cron.sh
+++ scripts/bind/cron.sh
@@ -8,7 +8,7 @@ else
 fi
 
 if [ "$3" = "" ]; then
-	echo "usage: $0 [--public] <dns-server> <zone> <domain> [domain] [...]"
+	echo "usage: $0 [--public] <dns-server[:port]> <zone> <domain> [domain] [...]"
 	exit 1
 elif ! [[ $1 =~ ^[a-z0-9.-]+[.][a-z0-9]+$ ]]; then
 	echo "error: parameter $1 not conforming hostname format"
@@ -17,7 +17,16 @@ fi
 
 server=$1
 inzone=$2
-key=`/opt/farm/ext/keys/get-ssh-dedicated-key.sh $server root`
+
+if [ -z "${server##*:*}" ]; then
+	host="${server%:*}"
+	port="${server##*:}"
+else
+	host=$server
+	port=22
+fi
+
+key=`/opt/farm/ext/keys/get-ssh-dedicated-key.sh $host root`
 shift
 shift
 
@@ -26,7 +35,7 @@ day=`date +%d`
 mon=`date +%m`
 year=`date +%Y`
 
-path=/var/cache/dns/$year/${year}${mon}/${year}${mon}${day}/$server/$hour
+path=/var/cache/dns/$year/${year}${mon}/${year}${mon}${day}/$host/$hour
 mkdir -p $path
 
 for domain in $@; do
@@ -35,8 +44,8 @@ for domain in $@; do
 	/opt/zonemanager/scripts/bind/generate-zone-file.php $inzone $domain $file $scope
 
 	if [ -s $file ]; then
-		scp -B -p -i $key -o StrictHostKeyChecking=no $file root@$server:/etc/bind
+		scp -B -p -i $key -P $port -o StrictHostKeyChecking=no $file root@$host:/etc/bind
 	fi
 done
 
-ssh -i $key root@$server /etc/init.d/bind9 restart >/dev/null
+ssh -i $key -p $port root@$host /etc/init.d/bind9 restart >/dev/null
